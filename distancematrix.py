2008d2a0825e9a03cc7f65d2e27a992380a13759
Aaheli Chattopadhyay <achattop7@gatech.edu>
2019-02-16 20:06:36 -0500

    added sort helpers by location function

--- distancematrix.py
+++ distancematrix.py
@@ -1,9 +1,48 @@
 import requests
-origins="Boston"
-destinations="Atlanta|Seattle"
-url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='+origins+'&destinations='+destinations+'&key=AIzaSyAIDJxS6sp_j8PJNk7y0RapKInxDBJkIz8'
-response = requests.post(url).json()
-print(len(response["rows"][0]['elements']))
-print(response["rows"][0]['elements'][0]['distance'])
-#print(response["rows"][0]['elements'][0]['distance'])
-print(response["rows"][0]['elements'][1]['distance'])
+import sys
+
+
+def sortHelpers(helpers):
+	origins="Chapel Hill,NC"
+	destinations=""
+	for h in helpers:
+		destinations=destinations+h["address"]+"|"
+	
+	destinations=destinations[:-1]
+
+	
+	#destinations="Atlanta|Seattle"
+	url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='+origins+'&destinations='+destinations+'&key=AIzaSyAIDJxS6sp_j8PJNk7y0RapKInxDBJkIz8'
+	response = requests.post(url).json()
+	
+	elems=response["rows"][0]['elements']
+	places=[]
+	for i in range(0,len(elems)):
+		dis=int((elems[i]['distance']['text'].replace(",","").replace(" mi","")))
+		places.append({"helper":helpers[i], "distance":dis})
+
+	def takeDistance(e):
+		return e["distance"]
+	places.sort(key=takeDistance)
+	sortedPlaces=[p["helper"] for p in places]
+	
+	return sortedPlaces
+
+
+
+
+def main(n):
+    helps=[{"name":"AC","address":"Boston,MA"},{"name":"BC","address":"Atlanta,GA"},
+    {"name":"CD","address":"New York,NY"}]
+    places=sortHelpers(helps)
+    print(places)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(int(sys.argv[1]))
+
+	
+
+	
+
+
